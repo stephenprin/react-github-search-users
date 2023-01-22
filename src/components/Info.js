f5b6328accc75ad0cf3c11882b09f53a12cabedc
stephenprin <stephenprince427@gmail.com>
2023-01-22 01:45:45 +0100

    user iinformation added fetching via api

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,10 +5,42 @@ import { GoRepo, GoGist } from 'react-icons/go';
 import { FiUsers, FiUserPlus } from 'react-icons/fi';
 
 const UserInfo = () => {
-  const data = useGlobalContext(); 
-  console.log(data);
-  return <h2>user info component</h2>;
+  const { githubUser } = useGlobalContext(); 
+  const { public_repos, followers, following, public_gists } = githubUser;
+  const items = [
+    { id: 1, icon: <GoRepo  />, label: 'repos', value: public_repos, color: 'pink' },
+    { id: 2, icon: <FiUsers />, label: 'followers', value: followers, color: 'green' },
+    { id: 3, icon: <FiUserPlus />, label: 'following', value: following, color: 'purple' },
+    { id: 4, icon: <GoGist/>, label: 'gists', value: public_gists, color: 'yellow' },
+  ]
+    
+  return (
+    <section className="section relative ">
+       <Wrapper className=' section-center'>
+       {items.map((item) => { 
+         const { id, icon, label, value, color } = item;
+         return (
+           <div className=' item py-[1rem] px-[2rem] rounded-lg bg-black ' key={id}>
+             <span className={color}>
+               {icon}
+              </span>
+               <div>
+                 <h3 className='mb-0'>{value}</h3>
+                  <p >{label}</p>
+                </div>
+               
+             
+            </div>
+          )
+    })}
+      </Wrapper>
+  </section>
+  
+   )
 };
+ 
+
+
 
 const Wrapper = styled.section`
   display: grid;
@@ -18,11 +50,10 @@ const Wrapper = styled.section`
     grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
   }
   .item {
-    border-radius: var(--radius);
-    padding: 1rem 2rem;
-    background: var(--clr-white);
+  
+  
     display: grid;
-    grid-template-columns: auto 1fr;
+   grid-template-columns: auto 1fr;
     column-gap: 3rem;
     align-items: center;
     span {
@@ -33,11 +64,12 @@ const Wrapper = styled.section`
       border-radius: 50%;
     }
     .icon {
-      font-size: 1.5rem;
+      font-size: 1.6rem;
     }
     h3 {
       margin-bottom: 0;
       letter-spacing: 0;
+      font-size: 1.2rem;
     }
     p {
       margin-bottom: 0;
@@ -48,16 +80,16 @@ const Wrapper = styled.section`
       color: #da4a91;
     }
     .green {
-      background: var(--clr-primary-10);
-      color: var(--clr-primary-5);
+      background: rgb(52 211 153);
+      color: rgb(167 243 208);
     }
     .purple {
       background: #e6e6ff;
       color: #5d55fa;
     }
-    .yellow {
-      background: #fffbea;
-      color: #f0b429;
+    .yellow{
+      background: rgb(251 191 36);
+      color: rgb(254 243 199);
     }
   }
 `;
