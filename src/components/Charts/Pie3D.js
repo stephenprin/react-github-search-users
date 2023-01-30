a5c989a4fa947adefe90b930cb258367e3073b1a
stephenprin <stephenprince427@gmail.com>
2023-01-30 08:59:59 +0100

    added chart to showing a github user stars achieved

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -13,7 +13,7 @@ import FusionCharts from "fusioncharts";
 import Column2D from "fusioncharts/fusioncharts.charts";
 
 // Include the theme as fusion
-import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
 
 // Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
@@ -26,16 +26,19 @@ const Pie3D = ({ data }) => {
   // STEP 3 - Creating the JSON object to store the chart configurations
 const chartConfigs = {
   type: "pie3d", // The chart type
-  width: "400", // Width of the chart
+  width: "100%", // Width of the chart
   height: "400", // Height of the chart
   dataFormat: "json", // Data type
   dataSource: {
     // Chart Configuration
     chart: {
       caption: "Languages",
-      theme: "fusion",
+      theme: "candy",
       decimals: 0,
       pieRadius: "40%",
+      
+     
+   
     },
     // Chart Data
     data
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -9,36 +9,34 @@ import {Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = useGlobalContext();
   const languages=repos.reduce((total, item) => { 
-    const { language, stargazers_count } = item;
+    const { language, stargazers_count  } = item;
     if (!language) return total;
     if (!total[language]) { 
-      total[language]={ label: language, value: 1}
+      total[language]={ label: language, value: 1, stars: stargazers_count}
     } else {
-      total[language] = { ...total[language], value: total[language].value + 1 } 
+      total[language] = { ...total[language], value: total[language].value + 1, stars:total[language].stars + stargazers_count } 
     }
     return total;
  
   }, {});
-  console.log(languages);/.,m 
-  const chartData = [
-    {
-      label: "HTML",
-      value: "18"
-    },
-    {
-      label: "CSS",
-      value: "42"
-    },
-    {
-      label: "Javascript",
-      value: "85"
-    },
-    
-  ];
+  console.log(languages);
+  const mostUsed = Object.values(languages).sort((a, b) => { 
+    return b.value - a.value;
+  }).slice(0, 5).reverse();
+
+  const mostPopular = Object.values(languages).sort((a, b) => {
+    return b.stars - a.stars;
+  }).map((item) => { 
+    return { ...item, value: item.stars }
+  } ).slice(0, 5);
+
   return (
     <section className='section'>
       <Wrapper className='section-center bg-gradient-to-r from-black to-gray-900'>
-        <Pie3D data={chartData} />
+        <Pie3D data={mostUsed} />
+        <Column3D data={mostPopular} />
+        <Doughnut2D  data={mostPopular} />
+        <div></div>
       </Wrapper>
    </section>
   )
@@ -58,14 +56,16 @@ const Wrapper = styled.div`
 
   div {
     width: 100% !important;
+  
   }
   .fusioncharts-container {
     width: 100% !important;
+   
   }
   svg {
     width: 100% !important;
     border-radius: var(--radius) !important;
-  }
+  } 
 `;
 
 export default Repos;
